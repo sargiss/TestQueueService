79e74ae640fea42267f1db745a30aaca0bd0ab46
sargiss <sagajanov@hotmail.com>
2014-12-08 04:25:55 +0300

    developing

---
+++ Queue/OperatorSessionRequestHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Queue.Communication;
+using Newtonsoft.Json;
+using Queue.Communication.Dto;
+
+namespace Queue
+{
+    class OperatorSessionRequestHandler: IRequestHandler
+    {
+        WorkerClient _client = new WorkerClient();
+
+        public ZMessage Process(ZMessage zmsg)
+        {
+            var body = zmsg.BodyToString();
+            var sessionEvt = JsonConvert.DeserializeObject<OperatorSessionEventMsg>(body);
+
+            zmsg.Push();
+        }
+    }
+}
